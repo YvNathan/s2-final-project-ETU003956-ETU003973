cff619f90952d587a9c5b5b7e09559076e4c7a97
Flo-rencia16 <dalilahandriamiaramanana@gmail.com>
2025-07-14 14:13:31 +0300

    fiche et histo obj

--- v2/ficheObjet.php
+++ v2/ficheObjet.php
@@ -15,12 +15,31 @@ $histo = charger_histo_emprunts($objet);
     <script src="../assets/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
+    <h2>Détails sur <?= $fiche ['nom_objet']?></h2>
     <img src="../assets/images/<?= $fiche['nom_image']?>" alt="">
-    <?php
-        foreach($histo as $h){?>
-            <?= $h['nom']?> 
-            <?= $h['date_emprunt']?>
-            <?= $h['date_retour']?>
-    <?php }?>
+    
+    <br>
+    
+    <h3>Historique de ses emprunts:</h3>
+    <table >
+        <thead>
+            <tr>
+                <th>Utilisateur</th>
+                <th>Date Emprunt</th>
+                <th>Date Retour</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php
+            foreach($histo as $h){?>
+                <tr>
+                    <td><?= $h['nom']?></td> 
+                    <td><?= $h['date_emprunt']?></td>
+                    <td><?= $h['date_retour']?></td>
+                </tr>
+            <?php }?>
+        </tbody>
+    </table>
+
 </body>
 </html>
